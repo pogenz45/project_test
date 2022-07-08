431bc773f5d691fc67931ecbdc04bd2702d002c8
anh vu <vutuan41550@gmail.com>
2022-07-08 13:22:01 +0700

    add recommened

---
+++ lib/values/app_images.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppImages {
+  static final String ImagePath = 'assets/images/';
+  static final String image1 = ImagePath + 'image1.jpg';
+  static final String image2 = ImagePath + 'image2.jpg';
+  static final String image3 = ImagePath + 'image3.jpg';
+}
